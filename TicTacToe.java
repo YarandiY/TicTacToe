a0aa3554b98c9049406b1670c3a2a6cab24fc8d0
Yalda <yalda.yarandi@gmail.com>
2019-05-10 04:40:09 +0430

    first commit

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 
+
+/**
+ * @author Yalda Yarandi
+ * tic-tac-toe game with AI (min-max algorithm)
+ */
+
+
+
 public class TicTacToe {
 
     private String[][] board;
@@ -18,6 +26,8 @@ public class TicTacToe {
     };
 
 
+
+    //main constructor
     public TicTacToe() {
         board = new String[3][3];
         for (int i = 0; i < 3; i++) {
@@ -27,6 +37,8 @@ public class TicTacToe {
         }
     }
 
+
+    //copy of a tictactoe board
     public TicTacToe(String[][] board) {
         this.board = new String[3][3];
         for (int i = 0; i < 3; i++) {
@@ -37,11 +49,17 @@ public class TicTacToe {
     }
 
 
+    /**
+     * @param index of slot
+     * @throws Exception : invalid input (out of bound)
+     * @throws Exception : The slot has been full
+     * for both of the exceptions the user must to re-enter slot number
+     */
     public void input(int index) throws Exception {
         if (index < 0 || index > 8)
             throw new Exception("Invalid input");
         if (!board[index / 3][index % 3].equals(String.valueOf(index))){
-            throw new Exception("already taken");
+            throw new Exception("The Slot has been full");
         }
         board[index / 3][index % 3] = turn;
         turn = turn.equals("X") ? "O" : "X";
@@ -52,16 +70,17 @@ public class TicTacToe {
         this.turn = turn;
     }
 
-    public String[][] getBoard() {
-        return board;
+    public String getTurn() {
+        return turn;
     }
+
 
     public static int[] minMax(TicTacToe ticTacToe) {
 
         int[] ans = new int[3];
 
         int minYAmax;
-        if (ticTacToe.turn.equals("X")) {
+        if (ticTacToe.getTurn().equals("X")) {
             minYAmax = Integer.MAX_VALUE;
         } else {
             minYAmax = Integer.MIN_VALUE;
@@ -70,12 +89,12 @@ public class TicTacToe {
         if (ticTacToe.endOfGame())
             return new int[]{-1, -1, ticTacToe.whoWin()};
 
-        int bwI = 0;
-        int bwJ = 0;
+        int bestWayI = 0;
+        int bestWayJ = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 TicTacToe tmp = new TicTacToe(ticTacToe.board);
-                tmp.turn = ticTacToe.turn;
+                tmp.setTurn(ticTacToe.getTurn());
                 try {
                     tmp.input(i * 3 + j);
                 } catch (Exception e) {
@@ -84,20 +103,20 @@ public class TicTacToe {
                     continue;
                 }
                 int value = minMax(tmp)[2];
-                if (ticTacToe.turn.equals("X") && minYAmax > value) {
+                if (ticTacToe.getTurn().equals("X") && minYAmax > value) {
                     minYAmax = value;
-                    bwI = i;
-                    bwJ = j;
-                } else if (ticTacToe.turn.equals("O") && minYAmax < value) {
+                    bestWayI = i;
+                    bestWayJ = j;
+                } else if (ticTacToe.getTurn().equals("O") && minYAmax < value) {
                     minYAmax = value;
-                    bwI = i;
-                    bwJ = j;
+                    bestWayI = i;
+                    bestWayJ = j;
                 }
             }
         }
 
-        ans[0] = bwI;
-        ans[1] = bwJ;
+        ans[0] = bestWayI;
+        ans[1] = bestWayJ;
         ans[2] = minYAmax;
 
         return ans;
@@ -137,43 +156,52 @@ public class TicTacToe {
 
 
     public void printBoard() {
-        System.out.println("--------------------");
-        System.out.println("-  " + board[0][0] + "  -  " + board[0][1] + "  -  " + board[0][2] + "  -");
-        System.out.println("-  " + board[1][0] + "  -  " + board[1][1] + "  -  " + board[1][2] + "  -");
-        System.out.println("-  " + board[2][0] + "  -  " + board[2][1] + "  -  " + board[2][2] + "  -");
-        System.out.println("--------------------");
+        System.out.println("-------------------");
+        System.out.println("|  " + board[0][0] + "  |  " + board[0][1] + "  |  " + board[0][2] + "  |");
+        System.out.println("-------------------");
+        System.out.println("|  " + board[1][0] + "  |  " + board[1][1] + "  |  " + board[1][2] + "  |");
+        System.out.println("-------------------");
+        System.out.println("|  " + board[2][0] + "  |  " + board[2][1] + "  |  " + board[2][2] + "  |");
+        System.out.println("-------------------");
     }
+
 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.turn = "X";
-        System.out.println("Start!!");
+
+        System.out.println("Welcome to one player Tic Tac Toe.");
+        System.out.println("X's will play first. Enter a slot number to place X in:");
+
+
         ticTacToe.printBoard();
+
+
         while (!ticTacToe.endOfGame()) {
-            int goTo;
+            int slot;
             if (ticTacToe.turn.equals("X"))
-                goTo = scanner.nextInt();
+                slot = scanner.nextInt();
             else {
                 int[] tmp = minMax(ticTacToe);
-                goTo = 3 * tmp[0] + tmp[1];
-                System.out.println(goTo);
+                slot = 3 * tmp[0] + tmp[1];
+                System.out.println(slot);
             }
             try {
-                ticTacToe.input(goTo);
+                ticTacToe.input(slot);
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
             ticTacToe.printBoard();
         }
 
-        if(ticTacToe.whoWin() == 1)
-            System.out.println("Excellent! You won the game... :)");
-        else if(ticTacToe.whoWin() == -1)
-            System.out.println("lose... :(");
+        if(ticTacToe.whoWin() == -1)
+            System.out.println("Congratulations! you have won! :)" + "\n" + "Thanks for playing.");
+        else if(ticTacToe.whoWin() == 1)
+            System.out.println("lose... :(( "+ "\n" + "Thanks for playing.");
         else
-            System.out.println("draw :|");
+            System.out.println("It's a draw! :|" + "\n" +"Thanks for playing.");
     }
 
 }
